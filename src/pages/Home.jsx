c60f71a436f5727fab3ccc255c3874e0b553e68a
senger-shubham <swapan.9057316@gmail.com>
2025-09-04 20:44:13 +0530

    fixed in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import image from "../assets/image.webp";
 const Home = () => {
   return (
     <section id='home'>
-    <div className='h-[850px] w-[100%] pt-[250px] bg-amber-500 text-white' style={{ backgroundImage:`url(${image})` , backgroundSize: "cover", backgroundPosition: "center"}}>
+    <div className='h-[850px] w-[100%] pt-[250px]  text-white' style={{ backgroundImage:`url(${image})` , backgroundSize: "cover", backgroundPosition: "center"}}>
     <div className='w-[50%] flex flex-col items-start pl-[200px] gap-6  text-3xl'>
       <div><p>Hello Its's Me</p></div>
       <div className='font-extrabold text-5xl'><p>Shubham Singh </p></div>
